729a87976c4e74ca7d14f8ecd96d9a91625e56b3
DubbelF34140 <yw34140@gmail.com>
2024-04-09 16:15:25 +0200

    FIX NavBar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,28 +5,39 @@ import Typography from '@mui/material/Typography';
 import UtilisateurService from "../../service/utilisateurService";
 import { Link } from 'react-router-dom';
 import authService from "../../service/auth-service";
-const Utilisateur = await UtilisateurService.getUtilisateurById()
 
 const logout = (event) => {
     authService.logout();
 };
-const Navbar = () => {
+const Navbar = ({ logout }) => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     ENI-Encheres
                 </Typography>
-                <Link to={`/profil`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
-                    Mon profil
-                </Typography>
-                </Link>
-                <Link to={`/`} style={{ textDecoration: 'none', color: 'inherit' }} onClick={logout}>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Déconnexion
-                    </Typography>
-                </Link>
+                {user ? (
+                    <>
+                        <Link to={`/profil`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+                                Mon profil
+                            </Typography>
+                        </Link>
+                        <Link to={`/`} style={{ textDecoration: 'none', color: 'inherit' }} onClick={logout}>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                Déconnexion
+                            </Typography>
+                        </Link>
+                    </>
+                ) : (
+                    <Link to={`/login`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+                            Connexion / Enregistrer
+                        </Typography>
+                    </Link>
+                )}
             </Toolbar>
         </AppBar>
     );
