b4f1915f3251591a3e17d7ec4a01759a592a9a60
DubbelF34140 <yw34140@gmail.com>
2024-04-10 15:02:41 +0200

    add enchère

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,56 +2,46 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import UtilisateurService from "../../service/utilisateurService";
+import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import authService from "../../service/auth-service";
 import AuthService from "../../service/auth-service";
-
-
 
 const logout = () => {
     AuthService.logout();
     sessionStorage.removeItem("user");
 };
+
 const Navbar = () => {
     const user = JSON.parse(sessionStorage.getItem('user'));
 
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography variant="h6" component="div">
                     ENI-Encheres
                 </Typography>
-                {user ? (
-                    <>
-                        <Link to={`/categorie/gestion`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+                <div>
+                    {user ? (
+                        <>
+                            <Button color="inherit" component={Link} to={`/categorie/gestion`}>
                                 Gérer les catégories
-                            </Typography>
-                        </Link>
-                        <Link to={`/article/add`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+                            </Button>
+                            <Button color="inherit" component={Link} to={`/article/add`}>
                                 Vendre un article
-                            </Typography>
-                        </Link>
-                        <Link to={`/profil`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+                            </Button>
+                            <Button color="inherit" component={Link} to={`/profil`}>
                                 Mon profil
-                            </Typography>
-                        </Link>
-                        <Link to={`/`} style={{ textDecoration: 'none', color: 'inherit' }} onClick={logout}>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            </Button>
+                            <Button color="inherit" component={Link} to={`/`} onClick={logout}>
                                 Déconnexion
-                            </Typography>
-                        </Link>
-                    </>
-                ) : (
-                    <Link to={`/login`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
+                            </Button>
+                        </>
+                    ) : (
+                        <Button color="inherit" component={Link} to={`/login`}>
                             Connexion / Enregistrer
-                        </Typography>
-                    </Link>
-                )}
+                        </Button>
+                    )}
+                </div>
             </Toolbar>
         </AppBar>
     );
